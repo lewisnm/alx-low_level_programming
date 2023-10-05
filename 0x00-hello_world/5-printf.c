5d3a94bf3f7cd2ca9267875a504fd8e03f16ddd3
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-05 16:17:54 -0700

    printing using print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: Prints with proper grammar, but the outcome is a piece of art,
+* Return: (0) - Successfully executed and exited
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
