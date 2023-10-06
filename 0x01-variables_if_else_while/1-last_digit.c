77c6ec134cc87e6cc2b629962ad9f27737973519
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-06 13:23:02 -0700

    showing greater than and less than

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,6 +24,7 @@ int main(void)
 	}
 	else if (n < 6)
 	{
-	printf("Last digit of %d is %d and is less than 0.\n", n, l);
+	printf("Last digit of %d is %d and is less than 6.\n", n, l);
+	}
 	return (0);
 }
