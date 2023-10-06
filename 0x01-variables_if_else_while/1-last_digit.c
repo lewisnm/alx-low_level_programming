efbe71d12860d4495363fd3829201ad5a2ec159f
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-06 12:08:55 -0700

    showing greater than and less than

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main- Entry point
+*Description- code tells whether n is greater, equal to or less than zero
+*Return:(0) success
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5.\n", n);
+	}
+	else if (n == 0)
+	{
+	printf("Last digit of %d is %d and is 0.\n", n);
+	}
+	else
+	{
+	printf("Last digit of %d is %d and is less than 0.\n", n);
+	return (0);
+}
