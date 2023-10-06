f3aa23e8fc24352ccea2b617171131a9190a9282
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-06 10:57:14 -0700

    How to print a random number with it value.

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.>
+
+/**
+*main- Entry point
+*Description- code picks a random number and states its integral value
+*Return:(0) success
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+/* for loop placing the n value into a class */
+	if (n == 0)
+	{
+/* if true print the following */
+	printf("n is zero\n");
+	}
+	else if (n < 0)
+	{
+/* if else if condition is true print the following */
+	printf("n is negative\n");
+	}
+	else
+	{
+/* if none of the conditions are met */
+	printf("n is positive\n");
+	}
+	return (0);
+}
