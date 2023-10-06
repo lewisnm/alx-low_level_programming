f943cba92ab82d8aff8497c0f5075be245bfa6d2
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-06 16:40:10 -0700

    all possible two digit combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description - does every possible combination of single numbers
+* Return: (0) success
+*/
+int main(void)
+{
+	int firstdigit = '0';
+
+	while (firstdigit <= '8')
+	{
+	int seconddigit = firstdigit + 1;
+
+	while (seconddigit <= '9')
+	{
+
+	putchar(firstdigit);
+	putchar(seconddigit);
+
+	if (firstdigit != 8 || seconddigit != '9')
+	{
+	putchar(',')
+	putchar(' ')
+	}
+	seconddigit++;
+	}
+
+	firstdigit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
