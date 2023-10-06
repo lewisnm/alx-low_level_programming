8aba1179a3adfc22ed663b3102502512c6ae1fc6
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-06 11:03:30 -0700

    How to print a random number with it value.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,17 +18,17 @@ int main(void)
 	if (n == 0)
 	{
 /* if true print the following */
-	printf("n is zero\n");
+	printf("%d is zero\n");
 	}
 	else if (n < 0)
 	{
 /* if else if condition is true print the following */
-	printf("n is negative\n");
+	printf("%d is negative\n");
 	}
 	else
 	{
 /* if none of the conditions are met */
-	printf("n is positive\n");
+	printf("%d is positive\n");
 	}
 	return (0);
 }
