67d795f28e6e52fa78ded86b014988b4c18c3493
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-06 12:51:17 -0700

    showing greater than and less than

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,21 +9,21 @@
 int main(void)
 {
 	int n;
-	int last;
+	int l;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last = n % 10;
+	l = n % 10;
 	if (n > 5)
 	{
-	printf("Last digit of {n} is %d and is greater than 5.\n", last, n);
+	printf("Last digit of {n} is %d and is greater than 5.\n", n, l);
 	}
 	else if (n == 0)
 	{
-	printf("Last digit of {n} is %d and is 0.\n", last, n);
+	printf("Last digit of {n} is %d and is 0.\n", n, l);
 	}
 	else if (n < 6)
 	{
-	printf("Last digit of {n} is %d and is less than 0.\n", last, n);
+	printf("Last digit of {n} is %d and is less than 0.\n", n, l);
 	return (0);
 }
