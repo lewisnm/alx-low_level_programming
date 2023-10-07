237645559054ec02930f00d9bdcf6a134355342c
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-06 18:20:36 -0700

    all possible two by two digit combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 
-	for (int num1 = 0; num1 <= 99; num1++)
+	for (int num1 = 0; num1 <= 98; num1++)
 	{
 	for (int num2 = num1 + 1; num2 <= 99; num2++)
 	{
 	putchar(num1 / 10 + '0');
 	putchar(num1 % 10 + '0');
 	putchar(' ');
-	putchar(num2 / 10i + '0');
+	putchar(num2 / 10 + '0');
 	putchar(num2 % 10) + '0');
 
 	if (!(num1 == 98 && num2 == 99)
