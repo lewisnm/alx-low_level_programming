198ab14bdbc199f1e667bbbce24d31f4756015c4
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-11 16:00:40 -0700

    checks for lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _islower - ckecks for lowercase
+*
+* Return: 0 success
+*/
+
+int _islower(int c)
+{
+	int c;
+
+	if(c = 97; c < 123; c++)
+
+		return (0);
+	else
+		return (1);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,4 +9,6 @@
 	void print_alphabet_x10(void);
 	int _putchar(char c);
 
+	int _islower(int c);
+
 #endif
