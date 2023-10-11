e1240af9e3397d38e244ea8e4fee673a7205e7d6
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-11 16:53:32 -0700

    checks for the sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_sign - checks for the sign of a number
+*
+* @n: The characetr being tested
+*
+* Return:  0 success
+*/
+
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+	else
+	{
+		putchar('+')
+		return (1);
+	}
+}
