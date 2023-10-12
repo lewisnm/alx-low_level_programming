261d437736c989c221977c261d2804aefc738cb7
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-12 05:46:25 -0700

    prints single nums

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,10 @@
 #ifndef my_header_file  
 #define my_header_file  
 	#include <stdio.h>
+	int _putchar(char c);
 	int _isupper(int c);
 	int _isdigit(int c);
 	int mul(int a, int b);
+	void print_numbers(void);
 
 #endif  
