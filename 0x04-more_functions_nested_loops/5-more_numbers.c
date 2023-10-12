4a29417c407735cd5d2dcfbdfddebb485a03c0d2
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-12 11:32:25 -0700

    prints 1-14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* more_numbers - prints numbers 10 times
+*
+* Return: void
+*/
+
+void more_numbers(void)
+{
+	int x;
+	int z;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (z = 0; z < 15; z++)
+		{
+			_putchar(z);
+		}
+		_putchar('\n');
+	}
+}
