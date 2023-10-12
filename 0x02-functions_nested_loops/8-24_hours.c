cb2a950e249d7dad31fdea856b54d7befb5eac8f
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-11 18:16:56 -0700

    all minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.c"
+
+/**
+* jack_bauer - all minutes of one day
+*
+* Return: void
+*/
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+
+			if (hour != 23 || minute != 59)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
+
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -18,5 +18,8 @@
 	int _abs(int);
 
 	int print_last_digit(int);
+	
+	void jack_bauer(void);
 
+	void times_table(void);
 #endif
