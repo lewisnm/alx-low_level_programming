0d8e20cbbe5c7a677226d687ccbfe43be02839af
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-12 04:37:35 -0700

    checks for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _isupper - checks for uppercase
+* @c: The character being tested
+* Return: Always 0
+*/
+
+int _isupper(int c)
+{
+	int c = 65;
+
+	if (c < 90)
+
+		return (1);
+
+	else
+
+		return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef my_header_file  
+#define my_header_file  
+	#include <stdio.h>
+	int _isupper(int c);
+
+#endif  
