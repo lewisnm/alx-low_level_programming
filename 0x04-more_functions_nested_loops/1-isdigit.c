d4bd1863d4cecee740b490c2de4b576255578bb8
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-12 04:59:41 -0700

    checks for numbers

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _isdigit - checks for dig 0-9
+* @c: The character being tested
+* Return: Always 0
+*/
+
+int _isdigit(int c)
+{
+	for (c = 0; c < 10; c++)
+
+		return (1);
+	else
+		return (0);
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define my_header_file  
 	#include <stdio.h>
 	int _isupper(int c);
+	int _isdigit(int c);
 
 #endif  
