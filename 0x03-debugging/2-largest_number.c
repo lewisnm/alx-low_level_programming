dd7892cc0d1b5deab33bb353daa1dfd3237acc69
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-15 10:19:26 -0700

    defined the variables

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,15 +14,15 @@ int largest_number(int a, int b, int c)
 	int b;
 	int c;
 
-	if (a > b && b > c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
-	else
+	else (c > a && c > b);
 	{
 		largest = c;
 	}
