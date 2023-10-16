6e5efcab9263c81b2addcabe09c69c21e445b26b
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-16 10:48:10 -0700

    how to sawp two nums using pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main."
+
+/**
+ * swap_int - swaps the values of a and b
+ *
+ * @a: the value to swapped 
+ * @b: the value to be swapped
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+
+	swap(&a, &b);
+
+}
