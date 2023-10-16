5e0c6a6ef26fa34fe09d63170f219bec5de7f731
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-16 06:57:28 -0700

    updating the value via a pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
-/*
- * resest_to _98 - updates the value to 98
- *
- * Return: void
- */
+/**
+* reset_to_98 - Function that updates a value to 98
+*
+* @n: pointer to value to be modified
+*
+* Return: void
+*/
 
 void reset_to_98(int *n)
 {
