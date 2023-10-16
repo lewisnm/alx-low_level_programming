59dd76eab062ac2a08742d874a1ee89278aa089e
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-16 11:34:46 -0700

    How  to count the string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int _strlen - determines the length of a string
+ *
+ * @s: the variable storing the string
+ *
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	char *s = str;
+	size_t length = strlen(s);
+}
