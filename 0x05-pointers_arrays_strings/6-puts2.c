866d34332509d11bc0fd5ea359f548b4bbb3a5fe
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-16 15:29:07 -0700

    prints every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -21,17 +21,9 @@ void puts2(char *str)
 	{
 		if (*str >= '0' && *str <= '9')
 		{
-
-			switch(digitCounter)
+			if (digit == digitCounter)
 			{
-				case 0:
-				case 2:
-				case 4:
-				case 6:
-				case 8:
-
-				_putchar(*str);
-				break;
+				putchar(*str);
 				digitCounter++;
 			}
 		}
