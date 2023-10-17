cb14ef1bac61ed8463a5a6f2fcfa3b0d0fbe28eb
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-17 13:51:57 -0700

    generates a key

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates a key
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int key[100];
+	int x, sum, y;
+	sum = 0;
+
+	strand(time (NULL));
+
+	for (x = 0; x < 100; x++)
+	{
+		key[x] = rand() % 78;
+		sum += (key[x] + '0');
+		putchar(key[x] + '0');
+
+		if ((2772 - sum) - '0' < 78)
+		{
+			y = 2772 - sum - '0';
+			sum += y;
+			putchar(y + '0');
+			break;
+		}
+	}
+}
