4cdd8fa9ff7a615e08ee6711b69dfc9767bc5016
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-16 18:18:28 -0700

    copies and and pasting using pointer

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,3 +8,17 @@
  *
  * Return: void
  */
+
+char *_strcpy(char *dest, char *src)
+{
+	int z;
+
+	for (z = 0; src[z] != '\0'; z++)
+	{
+		dest[z] = scr[z];
+	}
+	dest[z++] = '\0';
+
+	return (dest);
+}
+
