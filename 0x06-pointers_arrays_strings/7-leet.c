ecedfa91e2f624fa3fcbf8058e053380b682c172
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-19 14:57:39 -0700

    encoding strings

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string
+ *
+ * @n: string being encoded
+ *
+ * Return: n
+ */
+
+char *leet(char *n)
+{
+	int h, r;
+	char s1 [] = "aAeEoOtTlL";
+	char s2 [] = "4433007711";
+
+	for (h = 0; n[h] != '\0'; h++)
+	{
+		for (r = 0; r < 10; r++)
+		{
+			if (n[h] == s1[r])
+			{
+				n[h] = s2(r);
+			}
+		}
+	}
+	return (n);
+}
