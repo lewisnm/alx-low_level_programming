d7cc75a24d9c2cbb05d751e5f398b0fdd76e29a0
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-23 12:17:41 -0700

    fill a block of memory with a particular value

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset -  is used to fill a block of memory with a particular value
+ *
+ * @s: represents the pointer to the memory area.
+ * @b: represents the constant byte that will be used to fill the memory.
+ * @n: represents the number of bytes to fill.
+ *
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return s;
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef HEADER_FILE_NAME
+#define HEADER_FILE_NAME
+
+	char *_memset(char *s, char b, unsigned int n);
+#endif
