bb5a11d84eaa232ee9f5440acfb94ef175391884
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-25 11:56:20 -0700

    a string that prints a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,7 +11,10 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		return (0);
-	_putchar("%c\n", *s);
-	s++;
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
