8aee9aa7ef578dd9ebacc013dc26d6dd49c60c17
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-25 13:10:04 -0700

    prints a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,14 +3,14 @@
 /**
  * _print_rev_recursion - prints the string in reverse
  *
- * @s: the string being pointed at 
+ * @s: the string being pointed at
  *
  * Return: void
  */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
 		_putchar('\n');
 		return;
