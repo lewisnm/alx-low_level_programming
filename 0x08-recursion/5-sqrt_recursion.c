7196e918f7a9a7f21842df1b41069560758eb014
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-25 15:33:50 -0700

    how to print natural suare roots

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+int actual_formula(int n, int p);
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: 0
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (actual_formula(n, 2));
+}
+
+/**
+ * actual_formula - returns the natural square root of a number.
+ * @n: input number.
+ * @p: iterator.
+ * Return: square root or -1.
+ */
+int actual_formula(int n, int p)
+{
+	if (p % (n / p) == 0)
+	{
+		if (p* (n / p) == n)
+			return (p);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, p + 1));
+}
