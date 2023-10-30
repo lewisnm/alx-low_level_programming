5ee764c630afbf8eaa1ba01e601afa656564bca2
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-10-30 16:11:46 -0700

    prints current function name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ * main - prints out the nameof the function
+ *
+ * @argc: indexes the arguments
+ * @argv: prints out the specific string of an index
+ *
+ * return: success
+ */
+
+int main(int argc, char *argv[])
+{
+	_putchar(argv[0]);
+	_putchar("\n");
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef  HEADER_FILE
+#define HEADER_FILE
+
+	#include <stdio.h>
+	int _putchar(char c);
+	int main(int argc, char *argv[]);
+
+#endif
