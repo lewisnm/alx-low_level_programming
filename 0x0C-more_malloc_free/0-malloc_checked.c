51005c61f5577e393963d9022ce525b6aff9598c
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-02 15:45:21 -0700

    gives the address where the malloc is stored in memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * malloc_checked - Allocates memory
+  * @b: the size to allocate
+  *
+  * Return: void.
+  */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
