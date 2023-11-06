3f73ff284a449d1f4ada8c69288697f942673241
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-06 15:51:21 -0800

    initializes the dogs details

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,7 +24,7 @@ struct dog
 
 typedef struct dog dog_t;
 
-#include <stdio.h>
+#include <stdlib.h>
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
