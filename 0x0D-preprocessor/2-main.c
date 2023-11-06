925949dcdfb75f7ded4d4c5fd0f7947505ac27e4
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-06 07:12:41 -0800

    file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,6 +2,12 @@
 #define FILE_NAME_H
 #include <stdio.h>
 
-#define FILE_NAME  printf("The file name is %s\n," __FILE__)
+#define FILE_NAME __FILE__
+
+int main()
+{
+	printf("The file name is %s\n," __FILE__);
+	return (0);
+}
 
 #endif
