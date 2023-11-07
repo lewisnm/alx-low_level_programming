99d1351fd346815bfefc13117535c7122ba14abd
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-07 15:04:41 -0800

    creating a new file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,7 +20,6 @@ struct dog
 typedef struct dog dog_t;
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
