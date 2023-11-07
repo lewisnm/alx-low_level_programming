5a58d83bc291e789a96aa79055c417d8a89d2dc9
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-06 16:07:50 -0800

    prints the dogs details

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -25,6 +25,7 @@ struct dog
 typedef struct dog dog_t;
 
 #include <stdlib.h>
+#include <stdio.h>
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
