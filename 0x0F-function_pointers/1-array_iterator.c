fd57800aa77f7372689ae8650556f56e565b1bab
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-08 14:54:59 -0800

    A function that executes a function given as a parameter

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,20 +1,21 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - function executes given function as parameter
- * @array: an array of elements
- * @size: the size of array
- * @action: function of point
- * Return: void meaning nothing is returned
+ * array_iterator - a function that executes a function
+ * @array: the array
+ * @size: numbe rof elements
+ * @action: pointer to the function that needs to be used
+ * Return: Nothing
  */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int j;
 
 	if (!array || !action)
-    		return;
+		return;
 
-	for (j = 0; x < size; j++)
+	for (j = 0; j < size; j++)
+	{
 		action(array[j]);
+	}
 }
