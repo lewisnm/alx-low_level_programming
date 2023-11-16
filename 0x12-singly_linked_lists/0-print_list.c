6ac8f2d0d042a7d4fdc28e938872b5646d258268
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-16 07:19:50 -0800

    using singly lists to print out a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - prints the elements in our singly list
+ * @h: the number of arguments
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node = 0;
+
+	while (h != NULL)
+	{
+		if (h == NULL)
+		{
+			printf ("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s \n",h->len, h->str);
+			h = h->next;
+			node++;
+		}
+	}
+	return(node);
+}
