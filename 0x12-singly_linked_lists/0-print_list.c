a0b3b08d793a803145b736ec0a46bd573c6a5923
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-16 07:29:34 -0800

    using singly lists to print out a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,14 +14,14 @@ size_t print_list(const list_t *h)
 	{
 		if (h == NULL)
 		{
-			printf ("[%d] %s\n", 0, "(nil)");
+			printf("[%d] %s\n", 0, "(nil)");
 		}
 		else
 		{
-			printf("[%d] %s \n",h->len, h->str);
+			printf("[%d] %s \n", h->len, h->str);
 			h = h->next;
 			node++;
 		}
 	}
-	return(node);
+	return (node);
 }
