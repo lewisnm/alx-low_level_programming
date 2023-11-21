3d1594ecf7f6cc87760f1fb2c4dd474dd41c00b7
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-21 07:27:40 -0800

    prints out the elements in a singly linked list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe -prints linked lists 
+ * @head: pointer to list to be printed
+ *
+ * Return: new_node
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *temp = NULL;
+	const listint_t *list = NULL;
+	size_t count = 0;
+	size_t nu_list;
+
+	temp = head;
+	while (temp)
+	{
+		printf("[%p] %d\n",(void *)temp, temp->n);
+		count++;
+		temp = temp->next;
+		list = head;
+		nu_list = 0;
+		while (nu_list < count)
+		{
+			if (temp == list)
+			{
+				printf("-> [%p] %d\n",(void *)temp, temp->n);
+				return (count);
+			}
+			list = list->next;
+			nu_list++;
+		}
+		if (!head)
+			exit(98);
+	}
+	return (count);
+}
