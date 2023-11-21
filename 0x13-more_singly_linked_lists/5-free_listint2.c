97e9d132db4778653dca4af22de289918f475121
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-21 04:11:53 -0800

    frees space and sets head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees space in memory heap
+ * @head - pointer to the list 
+ *
+ * Return: NULL
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	
+	if (!head)
+		return;
+	
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}	
