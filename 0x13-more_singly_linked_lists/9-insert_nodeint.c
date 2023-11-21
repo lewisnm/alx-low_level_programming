a042d6903f9d809515edd180d9cc2b856707e204
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-21 06:17:33 -0800

    added an if statement in the while

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,8 +39,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (j < (idx - 1))
 	{
+		if (!temp || !temp->next)
+		{
+			free(nunode);
+			return (NULL);
+		}
 		temp = temp->next;
-		j++;
+		j++
 	}
 	nunode->next = temp->next;
 	temp->next = nunode;
