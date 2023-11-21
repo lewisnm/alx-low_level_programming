eb1d043f23cd2c639357789029807e5a207b0f48
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-21 08:26:45 -0800

    frees space

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees space allocated in the heap
+ * @h: this is the pointer to list
+ *
+ * Return:number of elements in the freed list
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t girth = 0;
+	int subt;
+	listint_t *temp;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		subt = *h - (*h)->next;
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			girth++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			girth++;
+			break;
+		}
+	}
+	*h = NULL;
+
+	return (girth);
+}
