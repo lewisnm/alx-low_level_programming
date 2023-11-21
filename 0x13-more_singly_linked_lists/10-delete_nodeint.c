0c157c7a4e8fa2240ce36e01dc5b2a92834ad1bb
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-21 06:06:48 -0800

    deletes a specific node in list

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes a code at a specific index
+ * @head: pointer to the list
+ * @index: index where the code should be deleted 
+ *
+ * Return: 1 for succes and -1 for fail
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp = *head;
+	listint_t *inx = NULL;
+	size_t j = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+	while (j < index -1)
+	{
+		if (!temp || !(temp->next))
+			return (-1);
+
+		temp = temp->next;
+		j++;
+	}
+	inx = temp->next;
+	temp->next = inx->next;
+	free(inx);
+
+	return (1);
+}
