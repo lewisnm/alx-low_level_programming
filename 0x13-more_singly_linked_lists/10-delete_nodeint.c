4bb984512597469297b29ddc23658ba776cbfe7b
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-21 06:09:18 -0800

    fixed the betty issue in number 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -3,7 +3,7 @@
 /**
  * delete_nodeint_at_index - deletes a code at a specific index
  * @head: pointer to the list
- * @index: index where the code should be deleted 
+ * @index: index where the code should be deleted
  *
  * Return: 1 for succes and -1 for fail
  */
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(temp);
 		return (1);
 	}
-	while (j < index -1)
+	while (j < index - 1)
 	{
 		if (!temp || !(temp->next))
 			return (-1);
