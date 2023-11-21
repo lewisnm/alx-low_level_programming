99106cf19b8a53b6d9b6014904bf944d69025a3b
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-21 05:23:00 -0800

    adds a node a specified point in the list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a node at the specified position
+ * @head: the beginning of the list
+ * @idx:where the node should be added
+ * @n: the data for the new node
+ *
+ * Return: address of the new node or Null
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *nunode, *temp = *head;
+	size_t j = 0;
+
+	nunode = malloc(sizeof(listint_t));
+	if (!node)
+		return (NULL);
+
+	nunode->n = n;
+	nunode->next = NULL;
+
+	else if (!*head && idx)
+		return (NULL);
+
+	else if (!idx)
+	{
+		nunode->next = temp;
+		*head = nunode;
+		return (nunode);
+	}
+	while (j < (idx - 1));
+	{
+		temp = temp->next;
+		j++;
+	}
+	nunode->next = temp->next;
+	temp->next = nunode;
+
+	return (node);
+}
