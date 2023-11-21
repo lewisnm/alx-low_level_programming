9662d339f0460a5291f10b06f12a508b87947fcd
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-21 06:19:25 -0800

    added a semi colon to j++

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -45,7 +45,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		}
 		temp = temp->next;
-		j++
+		j++;
 	}
 	nunode->next = temp->next;
 	temp->next = nunode;
