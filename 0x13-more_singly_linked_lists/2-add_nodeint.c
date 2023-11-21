6a8711893e6f45c12c9891d4e4da68886d5575f9
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-21 01:59:17 -0800

    fixed the syntax of malloc

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
 
-	new_node = malloc(size of(listint_s));
+	new_node = malloc(sizeof(listint_s));
 	if (!new_node || !head)
 	{
 		return (NULL);
