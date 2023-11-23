2313805dd151e1ffcacc29d55180ddb2887fbf3b
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-23 13:36:28 -0800

    adds a zero at a specified point in code

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - Sets the value of a bit at a given index to 0.
+ * @n: A pointer to the bit.
+ * @index: The to where we can set 0
+ *
+ * Return: -1 incase of error and 1 if it succeds
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	*n &= ~(1 << index);
+
+	return (1);
+}
