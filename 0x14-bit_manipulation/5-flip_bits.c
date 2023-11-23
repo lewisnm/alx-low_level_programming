645a3ff476a3baa1503b3a2b02cb0ba9e60d9b72
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-23 13:48:11 -0800

    returns the number of bits you would need to flip to get to the next

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the no. of bits to flip to get to the nex no.
+ * @n: The number 
+ * @m: The expected number to be flipped
+ *
+ * Return: The number of bits flipped
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int exlOr = n ^ m, bitsFlipped = 0;
+
+	while (exlOr > 0)
+	{
+		bitsFlipped += (exlOr & 1);
+		exOr >>= 1;
+	}
+
+	return (bitsFlipped);
+}
