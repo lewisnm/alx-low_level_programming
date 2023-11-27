1c59101d8d67def865cfa109218899bec412cce9
Lewis Mutiso <lewisnmutiso@gmail.com>
2023-11-27 10:58:26 -0800

    copies the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+  * main - Copies the contents of a file to another file
+  * @argc: number of arguments supplied to the program
+  * @argv: an array of pointers to the arguments
+  *
+  * Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	int file_des1, file_des2, sto;
+	char bufa[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	}
+	file_des1 =  open(argv[1], O_RDONLY, 0);
+	if (file_des1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	file_des2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (file_des2 == 1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+	while ((sto = read(file_des1, bufa, 1024)) > 0)
+	{
+		if (write(file_des2, bufa, sto) != sto)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		}
+	}
+	if (sto == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (close(file_des1) != 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file1), exit(100);
+	}
+	if (close(file_des2) != 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file2), exit(100);
+	}
+	return (0);
+}
