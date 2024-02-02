50a106522d2354ec337caae4c6320e4d705047ed
Lewis Mutiso <lewisnmutiso@gmail.com>
2024-02-02 02:49:13 -0800

    made significant changes to both files

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,31 +2,26 @@
 
 /**
  * hash_table_create - creates a hash table
- * @size - the array size of the hash table
+ * @size: size of the array
  *
- * Return: pointer to hash table
+ * Return: pointer to the newly created hash table
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *hash_table;
 	unsigned long int i;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-	{
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
 		return (NULL);
-	}
-	ht->size = size;
-	ht->table = malloc(size * sizeof(HashNode *));
-	if (ht->table == NULL)
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
 	{
-		free(ht);
+		free(hash_table);
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
-	{
-		ht->table[i] = NULL;
-	}
-	return (ht);
+		hash_table->array[i] = NULL;
+	return (hash_table);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -6,34 +6,35 @@
 #include <string.h>
 
 /**
- * struct HashNode_s - Node of a hash table
+ * struct hash_node_s - Node of a hash table
  *
  * @key: The key, string
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
  */
-typedef struct HashNode_s
+typedef struct hash_node_s
 {
      char *key;
      char *value;
-     struct HashNode_s *next;
-} HashNode;
+     struct hash_node_s *next;
+} hash_node_t;
 
 /**
- * struct - Hash table data structure
+ * struct hash_table_s - Hash table data structure
  *
  * @size: The size of the array
- * @table: An array of size @size
+ * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
  */
-typedef struct table_hash
+typedef struct hash_table_s
 {
      unsigned long int size;
-     HashNode **table;
+     hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
+
 
 #endif  /*HASH_TABLE*/
